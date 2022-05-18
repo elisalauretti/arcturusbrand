098e4042580a232e1c0337a636bda6ffce5805e8
elisalauretti <elisalauretti98@gmail.com>
2022-05-18 18:03:28 +0200

    accordion funzionanti

--- accordionmobile.js
+++ accordionmobile.js
@@ -4,12 +4,12 @@ const close1mob = document.querySelector(".close1mob");
 const panel1mob = document.querySelector(".panel-1mob");
 
 
-open1.addEventListener("click", openpanel1mob);
-close1.addEventListener("click", closepanel1mob);
+open1mob.addEventListener("click", openpanel1mob);
+close1mob.addEventListener("click", closepanel1mob);
 function openpanel1mob() {
     panel1mob.classList.remove("chiuso");
     panel1mob.classList.add("aperto");
-    open1mobclassList.add("attivo");
+    open1mob.classList.add("attivo");
     close1mob.classList.remove("attivo");
 }
 function closepanel1mob() {
